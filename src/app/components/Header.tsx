d4f1d88b5ac594bfc8779534b522e5a621b056f2
suzukeng <suzuken93@gmail.com>
2023-10-05 23:24:11 +0900

    ヘッダーにアイコンを追加

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,21 @@
 import NextLink from 'next/link'
-import { Box, Container, Heading, Flex } from '@chakra-ui/react'
+import { Box, Container, Heading, Flex, Image, HStack } from '@chakra-ui/react'
 export const Header = () => {
     return (
         <Box as='header' height='75px' px={6} bgColor='green.100'>
             <Container maxW='container.xxl'>
-                <Flex as="header" py="5" justifyContent="space-between" align="center">
-                    <NextLink href='/' passHref>
-                        <Heading as='h1' fontSize="2xl" cursor="pointer">
-                            SORT out THOUGHTs !
-                        </Heading>
-                    </NextLink>
-                </Flex>
+                <HStack>
+                    <Image src='/images/SORT-out-THOUGHTs.png' alt='SORT-out-THOUTs-Icon' width='40px' height='40px' />
+                    <Flex as="header" py="5" justifyContent="space-between" align="center">
+                        <NextLink href='/' passHref>
+                            <Heading as='h1' fontSize="3xl" cursor="pointer">
+                                SORT out THOUGHTs !
+                            </Heading>
+                        </NextLink>
+                    </Flex>
+                </HStack>
             </Container>
-        </Box>
+        </Box >
     )
 }
 export default Header
